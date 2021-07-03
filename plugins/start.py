fedef733742d1ab0ba30c34265d929b6c0bf51a8
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-07-03 13:46:23 +0530

    Initial Commit

--- plugins/start.py
+++ plugins/start.py
@@ -1,4 +1,4 @@
-rom pyrogram import Client, filters
+from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from database.blacklist import check_blacklist
